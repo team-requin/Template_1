08a232a97007f7229f87d1c6b368e239d74c003a
이성진 <chlskatnr1@naver.com>
2019-06-19 07:26:41 +0900

    Add css, boardwrite
    cdss

--- board/board_write.php
+++ board/board_write.php
@@ -4,8 +4,31 @@ $img_dir = "../imgs";
 include('../header.php');
 ?>
     <div id="board_write">
-        <div>asd</div>
-    </div>
+        <div>
+            <div id="board_area">
+                <form>
+                    <table>
+                        <tbody>
+                            <tr id="board_title">
+                                <th>제목</th>
+                                <td><input type="text" placeholder="제목을 입력해주세요."></td>
+                            </tr>
+                            <tr id="board_input">
+                                <th>글 쓰기</th>
+                                <td><input type="text" placeholder="글을 쓰세요."></td>
+                            </tr>
+                            <tr id="board_option">
+                                <td>
+                                    비공개<input id="board_non" type="checkbox" value="비공개">
+                                    <button id="board_check">확인</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+        </div>
+    </div><!--옵션 제목 내용 링크-->
 
 <?php
 include('../footer.php');
