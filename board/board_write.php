59a72e98c7cea7dd04bbaf813f154e4a5a1f61ef
이성진 <chlskatnr1@naver.com>
2019-06-19 15:15:51 +0900

    add css
    board write

--- board/board_write.php
+++ board/board_write.php
@@ -7,28 +7,30 @@ include('../header.php');
         <div>
             <div id="board_area">
                 <form>
-                    <table>
-                        <tbody>
-                            <tr id="board_title">
-                                <th>제목</th>
-                                <td><input type="text" placeholder="제목을 입력해주세요."></td>
-                            </tr>
-                            <tr id="board_input">
-                                <th>글 쓰기</th>
-                                <td><input type="text" placeholder="글을 쓰세요."></td>
-                            </tr>
-                            <tr>
-                                <th>링크</th>
-                                <td><input type="text" placeholder="링크를 입력해주세요."></td>
-                            </tr>
-                            <tr id="board_option">
-                                <td>
-                                    비공개<input id="board_non" type="checkbox" value="비공개">
-                                    <button id="board_check">확인</button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div id="table_div">
+                        <table>
+                            <tbody>
+                                <tr id="board_title">
+                                    <th>제목</th>
+                                    <td><input type="text" placeholder="제목을 입력해주세요."></td>
+                                </tr>
+                                <tr id="board_input">
+                                    <th>글 쓰기</th>
+                                    <td><input type="text" placeholder="글을 쓰세요."></td>
+                                </tr>
+                                <tr>
+                                    <th>링크</th>
+                                    <td><input type="text" placeholder="링크를 입력해주세요."></td>
+                                </tr>
+                                <tr id="board_option">
+                                    <td>
+                                        비공개<input id="board_non" type="checkbox" value="비공개">
+                                        <button id="board_check">확인</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </form>
             </div>
         </div>
