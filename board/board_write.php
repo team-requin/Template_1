b042aad82d4a726f548caa4fbf508b6faf51dc44
이성진 <chlskatnr1@naver.com>
2019-06-19 21:27:36 +0900

    board_write, css
    add, fix

--- board/board_write.php
+++ board/board_write.php
@@ -5,25 +5,28 @@ include('../header.php');
 ?>
     <div id="board_write">
         <form id="board_frame">
-            <table cellspacing="0">
+            <table cellspacing="0" cellpadding="0">
                 <tbody id="board_tbody">
                     <tr>
                         <th>제목</th>
-                        <td>제목 공간</td>
+                        <td><input id="board_title"type="text" placeholder="제목을 입력해주세요"></td>
                     </tr>
                     <tr>
                         <th>내용</th>
-                        <td>내용 공간</td>
+                        <td><pre>내용</pre></td>
                     </tr>
                     <tr>
                         <th>옵션</th>
-                        <td>링크, 비공개 책박</td>
+                        <td>
+                            <input id="board_link"type="text" placeholder="링크를 입력해주세요">
+                            <input id="board_check" type="checkbox" value="board_check">
+                            <label for="board_check">비공개</label>
+                        </td>
                     </tr>
                 </tbody>
             </table>
         </form>
     </div>
-    <!--옵션 제목 내용 링크-->
 <?php
 include('../footer.php');
 ?>
