86ff7bd6e8f85431a3c7faf712172357b2e123c5
이성진 <chlskatnr1@naver.com>
2019-06-19 18:47:35 +0900

    board_write, css
    css

--- board/board_write.php
+++ board/board_write.php
@@ -4,38 +4,26 @@ $img_dir = "../imgs";
 include('../header.php');
 ?>
     <div id="board_write">
-        <div>
-            <div id="board_area">
-                <form>
-                    <div id="table_div">
-                        <table>
-                            <tbody>
-                                <tr id="board_title">
-                                    <th>제목</th>
-                                    <td><input type="text" placeholder="제목을 입력해주세요."></td>
-                                </tr>
-                                <tr id="board_input">
-                                    <th>글 쓰기</th>
-                                    <td><input type="text" placeholder="글을 쓰세요."></td>
-                                </tr>
-                                <tr>
-                                    <th>링크</th>
-                                    <td><input type="text" placeholder="링크를 입력해주세요."></td>
-                                </tr>
-                                <tr id="board_option">
-                                    <td>
-                                        비공개<input id="board_non" type="checkbox" value="비공개">
-                                        <button id="board_check">확인</button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div><!--옵션 제목 내용 링크-->
-
+        <form id="board_frame">
+            <table cellspacing="0">
+                <tbody id="board_tbody">
+                    <tr>
+                        <th>제목</th>
+                        <td>제목 공간</td>
+                    </tr>
+                    <tr>
+                        <th>내용</th>
+                        <td>내용 공간</td>
+                    </tr>
+                    <tr>
+                        <th>옵션</th>
+                        <td>링크, 비공개 책박</td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
+    <!--옵션 제목 내용 링크-->
 <?php
 include('../footer.php');
 ?>
