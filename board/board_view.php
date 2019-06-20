ffaf0d1262c183bd0f4543301faadce4192f5fca
이성진 <chlskatnr1@naver.com>
2019-06-20 21:54:10 +0900

    js, board_view

---
+++ board/board_view.php
@@ -0,0 +1,9 @@
+<?php
+include("header.php");
+
+$result = mysqli_fetch_array(mysqli_query($connect, "select * from board where board_id='".$board."'"));
+if(!$result[0]) {
+	alert("존재하지 않는 게시판입니다");
+	page("/");
+}
+?>
