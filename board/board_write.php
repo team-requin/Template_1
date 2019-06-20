ca43cbf97bbff4ad8509b92b34934bda1299504e
이성진 <chlskatnr1@naver.com>
2019-06-20 10:02:16 +0900

    board_write, css
    css

--- board/board_write.php
+++ board/board_write.php
@@ -5,7 +5,7 @@ include('header.php');
 ?>
     <div id="board_write">
         <form id="board_frame">
-            <table cellspacing="0" cellpadding="0">
+            <table cellspacing="0" cellpadding="0" >
                 <tbody id="board_tbody">
                     <tr>
                         <th>제목</th>
@@ -13,18 +13,29 @@ include('header.php');
                     </tr>
                     <tr>
                         <th>내용</th>
-                        <td><pre>내용</pre></td>
+                        <td>
+                            <textarea></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>링크</th>
+                        <td>
+                            <input id="board_link"type="text" placeholder="링크를 입력해주세요">
+                        </td>
                     </tr>
                     <tr>
                         <th>옵션</th>
                         <td>
-                            <input id="board_link"type="text" placeholder="링크를 입력해주세요">
                             <input id="board_check" type="checkbox" value="board_check">
-                            <label for="board_check">비공개</label>
+                            <label for="board_check">비공개</label>                        
                         </td>
                     </tr>
                 </tbody>
             </table>
+            <div id="board_button">
+                <button>작성완료</button>
+                <button>취소</button>
+            </div>
         </form>
     </div>
 <?php
---
+++ js/index.js
@@ -0,0 +1,9 @@
+function header_btn_lgin() {
+    alert('clicked1');
+}
+function header_btn_mypage() {
+    alert('clicked2');
+}
+function header_btn_lgout() {
+    alert('clicked3');
+}
