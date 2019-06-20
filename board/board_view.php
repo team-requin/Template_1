40665502eea5eb62295bcaef7514ae28e766d03d
LeagueLugas <blackblack9685@gmail.com>
2019-06-20 21:56:54 +0900

    Tmp Board

--- board/board_view.php
+++ board/board_view.php
@@ -3,7 +3,7 @@ include("header.php");
 
 $result = mysqli_fetch_array(mysqli_query($connect, "select * from board where board_id='".$board."'"));
 if(!$result[0]) {
-	alert("존재하지 않는 게시판입니다");
-	page("/");
+	//alert("존재하지 않는 게시판입니다");
+	//page("/");
 }
 ?>
