f21557a896f02a5af1443c98a1cd937c879d1883
LeagueLugas <blackblack9685@gmail.com>
2019-06-20 21:08:19 +0900

    Add JS

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<script type="text/javascript" src="/js/index.js"></script>
     <title>Title</title>
 	<style>
 		@import url(<?php echo $skin_dir; ?>/default.css) screen and (min-width: 950px);
