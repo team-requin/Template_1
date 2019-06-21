8133e5db0818b9c0df7ab48b3b3ef90daa8c8edf
LeagueLugas <blackblack9685@gmail.com>
2019-06-21 11:37:33 +0900

    Add Editor

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <head>
 	<meta charset="UTF-8">
 	<script type="text/javascript" src="/js/index.js"></script>
+	<script type="text/javascript" src="/plugins/Smart_Editor/js/HuskyEZCreator.js" charset="utf-8"></script>
     <title>Title</title>
 	<style>
 		@import url(<?php echo $skin_dir; ?>/default.css) screen and (min-width: 950px);
