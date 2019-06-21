daf71431cd80b7550dd297954fab05abce88c353
이성진 <chlskatnr1@naver.com>
2019-06-21 12:53:16 +0900

    add css, board_view write
    fix

--- board/board_view.php
+++ board/board_view.php
@@ -7,3 +7,75 @@ if(!$result[0]) {
 	//page("/");
 }
 ?>
+<div id="bo_v_container">
+	<article id="bo_v">
+		<header><h1 id="bo_v_title">제목</h1></header>
+		<div id="bo_v_if">
+			<section id="bo_v_info">
+				작성자 <strong><span class="sv_member">작성자 이름</span></strong>
+				<span class="sound_only">작성일</span>
+				<strong>19-05-25 21:21</strong>
+				<span class="if_r">
+					댓글<strong>0건</strong>
+				</span>
+			</section>
+		</div>
+
+		<div id="bo_v_top">
+			<ul class="bo_v_nb">
+				<li><a class="btn_b01">다음글</a></li>
+			</ul>
+			<ul class="bo_v_com">
+				<li><a class="btn_b01">수정</a></li>
+				<li><a class="btn_b01">삭제</a></li>
+				<li><a class="btn_b01">목록</a></li>
+				<li><a class="btn_b02">글쓰기</a></li>
+			</ul>
+		</div>
+
+		<section id="bo_v_atc">
+			<p id="bo_v_atc_title">안녕</p>
+			<div id="bo_v_img"></div>
+			<div id="bo_v_con">
+				<p></p>
+			</div>
+		</section>
+		
+		<section id="bo_vc">
+			<h2>댓글목록</h2>
+				<div id="bo_vc_comment">
+					<header>
+						<span>작성자 이름</span>
+						작성일<time>19-06-21 11:12</time>
+					</header>
+					<p>댓글</p>
+				</div>
+				<p id="bo_vc_empty">등록된 댓글이 없습니다.</p>
+		</section>
+
+		<aside id="bo_vc_w">
+			<h2>댓글쓰기</h2>
+			<form name="fviewcomment" autocomplete="off">
+				<div class="tbl_wrap tbl_cmt">
+				<table>
+				<tbody>
+				<tr>
+					<th scope="row">내용</th>
+					<td colspan="5">
+						<div id="wr_container">
+							<textarea id="wr_content" name="wr_content" maxlength="10000" required="" class="required" title="내용"></textarea>
+							<div class="btn_confirm">
+								<input type="submit" id="btn_submit" class="btn_submit" value="댓글등록">
+							</div>
+						</div>
+						<div class="cmt_scr"><input type="checkbox" name="wr_secret" value="secret" id="wr_secret"><label for="wr_secret">비밀글사용</label></div>
+						
+					</td>
+				</tr>
+				</tbody>
+				</table>
+				</div>
+			</form>
+		</aside>
+	</article>
+</div>
--- board/board_write.php
+++ board/board_write.php
@@ -27,7 +27,6 @@ include('header.php');
 								// 에디터의 내용이 textarea에 적용됩니다.
 								oEditors.getById["ir1"].exec("UPDATE_CONTENTS_FIELD", []);
 								// 에디터의 내용에 대한 값 검증은 이곳에서 document.getElementById("ir1").value를 이용해서 처리하면 됩니다.
-					 
 								try {
 									elClickedObj.form.submit();
 								} catch(e) {}
